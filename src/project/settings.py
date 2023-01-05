49f22fefe483bf7adb074d584e2c48fe9f2434e1
Chougrani Sidi Mohammed <chougranior31@gmail.com>
2023-01-05 21:44:17 +0100

    ddb

--- src/project/settings.py
+++ src/project/settings.py
@@ -97,10 +97,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database-doctodz',
+        'USER': 'postgres',
+        'PASSWORD': 'mohammed21061992mM',
+        'HOST': 'doctodzdb.cyz3wwfinmib.eu-west-3.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
